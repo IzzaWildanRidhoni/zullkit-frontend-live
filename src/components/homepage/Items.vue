e89d67d878ac543b071ec2f84f30e36bcca734d4
Izza wildan ridhoni <izzawildan88@gmail.com>
2023-12-05 11:25:13 +0700

    Menerapkan API Categories Dan Products  Ke Vue Js

--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -1,19 +1,30 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import ItemCard from './../ItemCard.vue';
+import axios from 'axios';
 
-const items = ref([
-    { id: 1, title: 'Mobile UI Kit', description: 'Mobile UI Kit', image: 'items-1.jpg' },
-    { id: 2, title: 'Online Doctor Consultation', description: 'Online Doctor Consultation', image: 'items-2.jpg' },
-    { id: 3, title: 'Banking Crypto', description: 'Banking Crypto', image: 'items-3.jpg' },
-]);
+const items = ref([]);
+
+async function getItemsData() {
+    try {
+        const response = await axios.get('https://zullkit-backend.belajarkoding.com/api/products');
+        console.log(response.data.data.data);
+        items.value = response.data.data.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+onMounted(() => {
+    getItemsData();
+})
 </script>
 <template>
     <div class="container px-4 mx-auto my-16 md:px-12">
         <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Item</h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.title" :description="item.description"
-                :image="item.image" />
+            <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.name" :description="item.subtitle"
+                :image="item.thumbnails" />
         </div>
     </div>
 </template>
